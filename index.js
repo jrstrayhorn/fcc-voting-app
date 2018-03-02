bdfca156bc1975bebbf0040ddbf21c5d498ed971
stepforwardtech <jrstrayhorn@gmail.com>
2018-03-02 18:07:52 -0500

    Setting up express to work with create react app; added heroku postbuild

--- index.js
+++ index.js
@@ -21,5 +21,19 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Telling express to serve up assets from
+  // Create React App production build
+  app.use(express.static('client/build'));
+
+  // If express got here then it can't find the
+  // asset, serving up index.html so react-router
+  // takes over
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__direname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
