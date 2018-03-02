d7c0af83d1117b0cf4f3a29953cf33981e498a69
stepforwardtech <jrstrayhorn@gmail.com>
2018-03-02 16:55:17 -0500

    Finished authenticate - login/logout user story

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="left brand-logo">
+            FCC Voting App
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return {
+    auth
+  };
+}
+
+export default connect(mapStateToProps)(Header);
